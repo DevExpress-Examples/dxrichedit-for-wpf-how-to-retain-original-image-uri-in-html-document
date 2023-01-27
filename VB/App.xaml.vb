ecab92af6f0b98f1e0157bc1644769ff13364395
DevExpressExampleBot <fakeEmail>
2023-01-27 12:12:04 +0400

    Source auto update [skip ci]

---
+++ VB/App.xaml.vb
@@ -0,0 +1,12 @@
+Imports System.Windows
+
+Namespace Retain_Img_Src
+
+    ''' <summary>
+    ''' Interaction logic for App.xaml
+    ''' </summary>
+    Public Partial Class App
+        Inherits Application
+
+    End Class
+End Namespace
